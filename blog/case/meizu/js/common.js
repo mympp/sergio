cf61aa3e512739bd277c76479fb44f2a1f50d6e6
sergiolin <sergiolin@163.com>
2018-01-22 23:07:56 +0800

    add

---
+++ blog/case/meizu/js/common.js
@@ -0,0 +1,13 @@
+/*
+公共js
+ */
+// 公用头部
+$(document).on("mouseenter mouseleave",".mz-hover",function(){
+	$(".mz-box").slideToggle("show");
+})
+$(document).on("mouseenter mouseleave",".ml-hover",function(){
+	$(".ml-box").slideToggle("show");
+})
+$(document).on("mouseenter mouseleave",".pj-hover",function(){
+	$(".pj-box").slideToggle("show");
+})
